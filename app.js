455c8b188825d37113b0569a306422894c8b98db
Walter Huang <autumnhao@gmail.com>
2021-04-10 16:51:30 -0700

    Made some change to fundamental codebase to run on Heroku.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const _ = require('lodash');
 const express = require('express');
+const mongoose = require('mongoose');
+const { ID } = require('./config');
+const { KEY } = require('./config');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,6 +11,20 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+
+mongoose.connect(`mongodb+srv://${ID}:${KEY}@cluster0.zvnci.mongodb.net/userDB`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+
+const userSchema = mongoose.Schema({
+  email: String,
+  password: String
+});
+
+const User = new mongoose.model('user', userSchema);
+
 
 app.get('/', (req, res) => {
   res.render('home');
@@ -18,7 +35,24 @@ app.get('/login', (req, res) => {
 });
 
 app.get('/register', (req, res) => {
-res.render('register');
+  res.render('register');
+});
+
+app.post('/register', (req, res) => {
+
+  const newUser = new User({
+    email: req.body.userName,
+    password: req.body.password
+  });
+
+  newUser.save((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('secrets');
+    }
+  });
+
 });
 
 app.listen(port, () => {
