3a1daf0da54f07550f6e174205b030de01c01c78
Walter Huang <autumnhao@gmail.com>
2021-04-11 11:35:42 -0700

    Implement the bcrypt authentication method to encrypt the password.

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const _ = require('lodash');
 const express = require('express');
 const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
+const bcrypt = require('bcrypt');
 
+const saltRounds = 10;
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -61,19 +63,21 @@ app.route('/register')
   })
   .post((req, res) => {
 
-    const newUser = new User({
-      email: req.body.username,
-      password: req.body.password
-    });
+    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
 
-    newUser.save((err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render('secrets');
-      }
-    });
+      const newUser = new User({
+        email: req.body.username,
+        password: hash
+      });
 
+      newUser.save((err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.render('secrets');
+        }
+      });
+    });
   });
 
 app.listen(port, () => {
